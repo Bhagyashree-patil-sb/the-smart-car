3ed117e997e8706118b099b969313eb3a651c5df
Bhagyashree Patil <bhagyashree.patil1411sb@gmail.com>
2026-06-23 08:26:00 +0530

    Update smart-car.ino
    Signed-off-by: Bhagyashree Patil <bhagyashree.patil1411sb@gmail.com>

--- Arduino-code/smart-car/smart-car.ino
+++ Arduino-code/smart-car/smart-car.ino
@@ -1,245 +1,245 @@
-#include <SoftwareSerial.h>
-#include <Servo.h>
+#include <BluetoothSerial.h>
+#include <ESP32Servo.h>
 
-SoftwareSerial bluetooth(10, 11);
+BluetoothSerial SerialBT;
 Servo scanner;
 
-const int motorLeft1 = 2;
-const int motorLeft2 = 3;
-const int motorRight1 = 4;
-const int motorRight2 = 5;
+#define ENA 14
+#define IN1 27
+#define IN2 26
 
-const int speedA = 6;
-const int speedB = 9;
+#define ENB 12
+#define IN3 25
+#define IN4 33
 
-const int trigPin = 7;
-const int echoPin = 8;
+#define TRIG 5
+#define ECHO 18
 
-const int servoPin = 12;
+#define SERVO_PIN 13
 
-volatile long leftCount = 0;
-volatile long rightCount = 0;
+#define CH_A 0
+#define CH_B 1
 
-char cmd;
-int speedValue = 180;
-bool autoModeEnabled = false;
+char author[] = {83,117,114,101,115,104,32,83,104,101,114,105,107,97,114,0};
 
-long duration;
-int distanceValue;
+int motorSpeed = 180;
 
-const float wheelCircumference = 21.0;
-const int pulsesPerRevolution = 20;
+bool autoMode = false;
+bool routeMode = false;
+bool routeRunning = false;
+int routeID = 0;
 
-char authorName[] = {83,117,114,101,115,104,32,83,104,101,114,105,107,97,114,0};
+unsigned long lastCmdTime = 0;
 
-void leftEncoderISR()
-{
-  leftCount++;
+void setSpeed(int s) {
+  motorSpeed = s;
+  ledcWrite(CH_A, motorSpeed);
+  ledcWrite(CH_B, motorSpeed);
 }
 
-void rightEncoderISR()
-{
-  rightCount++;
+void stopCar() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
-long cmToPulses(float cm)
-{
-  return (cm / wheelCircumference) * pulsesPerRevolution;
+void forward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
-void moveForwardDistance(float cm)
-{
-  leftCount = 0;
-  rightCount = 0;
-  long target = cmToPulses(cm);
-  digitalWrite(motorLeft1, HIGH);
-  digitalWrite(motorLeft2, LOW);
-  digitalWrite(motorRight1, HIGH);
-  digitalWrite(motorRight2, LOW);
-  while (leftCount < target && rightCount < target) {}
-  stopRobot();
+void backward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
-void moveBackwardDistance(float cm)
-{
-  leftCount = 0;
-  rightCount = 0;
-  long target = cmToPulses(cm);
-  digitalWrite(motorLeft1, LOW);
-  digitalWrite(motorLeft2, HIGH);
-  digitalWrite(motorRight1, LOW);
-  digitalWrite(motorRight2, HIGH);
-  while (leftCount < target && rightCount < target) {}
-  stopRobot();
+void left() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
-void turnLeftDistance(int pulses)
-{
-  leftCount = 0;
-  rightCount = 0;
-  digitalWrite(motorLeft1, LOW);
-  digitalWrite(motorLeft2, HIGH);
-  digitalWrite(motorRight1, HIGH);
-  digitalWrite(motorRight2, LOW);
-  while (rightCount < pulses) {}
-  stopRobot();
+void right() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
-void turnRightDistance(int pulses)
-{
-  leftCount = 0;
-  rightCount = 0;
-  digitalWrite(motorLeft1, HIGH);
-  digitalWrite(motorLeft2, LOW);
-  digitalWrite(motorRight1, LOW);
-  digitalWrite(motorRight2, HIGH);
-  while (leftCount < pulses) {}
-  stopRobot();
-}
-
-void setup()
-{
-  pinMode(motorLeft1, OUTPUT);
-  pinMode(motorLeft2, OUTPUT);
-  pinMode(motorRight1, OUTPUT);
-  pinMode(motorRight2, OUTPUT);
-
-  pinMode(speedA, OUTPUT);
-  pinMode(speedB, OUTPUT);
-
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-  attachInterrupt(digitalPinToInterrupt(2), leftEncoderISR, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), rightEncoderISR, RISING);
-
-  Serial.begin(9600);
-  bluetooth.begin(9600);
-
-  scanner.attach(servoPin);
-  scanner.write(90);
-
-  stopRobot();
-
-  Serial.println("Robot is starting...");
-  Serial.print("Author: ");
-  Serial.println(authorName);
-}
-
-void loop()
-{
-  if (bluetooth.available())
-  {
-    cmd = bluetooth.read();
-    processCommand(cmd);
-  }
-
-  analogWrite(speedA, speedValue);
-  analogWrite(speedB, speedValue);
-
-  if (autoModeEnabled)
-  {
-    autoNavigation();
-  }
-}
-
-void processCommand(char command)
-{
-  switch (command)
-  {
-    case 'F': autoModeEnabled = false; moveForwardDistance(20); break;
-    case 'B': autoModeEnabled = false; moveBackwardDistance(20); break;
-    case 'L': autoModeEnabled = false; turnLeftDistance(15); break;
-    case 'R': autoModeEnabled = false; turnRightDistance(15); break;
-    case 'S': autoModeEnabled = false; stopRobot(); break;
-    case 'A': autoModeEnabled = true; break;
-    case '1': autoModeEnabled = false; runSchoolRoute(); break;
-    case '2': autoModeEnabled = false; runHomeRoute(); break;
-    case '3': autoModeEnabled = false; runOfficeRoute(); break;
-    case '4': autoModeEnabled = false; runHospitalRoute(); break;
-  }
-}
-
-int getDistance()
-{
-  digitalWrite(trigPin, LOW);
+long getDistance() {
+  digitalWrite(TRIG, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(TRIG, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH, 20000);
-  if (duration == 0) return 999;
-  return duration * 0.034 / 2;
+  digitalWrite(TRIG, LOW);
+  long duration = pulseIn(ECHO, HIGH, 30000);
+  long dist = duration * 0.034 / 2;
+  return dist;
 }
 
-void autoNavigation()
-{
-  distanceValue = getDistance();
-  if (distanceValue > 20)
-  {
-    moveForwardDistance(10);
+void moveForward(int ms) {
+  forward();
+  delay(ms);
+  stopCar();
+}
+
+void turnLeft(int ms) {
+  left();
+  delay(ms);
+  stopCar();
+}
+
+void turnRight(int ms) {
+  right();
+  delay(ms);
+  stopCar();
+}
+
+void autoDrive() {
+  long front;
+  scanner.write(90);
+  delay(200);
+  front = getDistance();
+
+  if (front > 25 || front == 0) {
+    forward();
     return;
   }
-  stopRobot();
-  moveBackwardDistance(10);
+
+  stopCar();
+  delay(200);
+
   scanner.write(150);
   delay(300);
-  int leftSide = getDistance();
+  long leftD = getDistance();
+
   scanner.write(30);
   delay(300);
-  int rightSide = getDistance();
+  long rightD = getDistance();
+
   scanner.write(90);
-  if (leftSide > rightSide)
-  {
-    turnLeftDistance(15);
+
+  if (leftD > rightD) {
+    left();
+    delay(400);
+  } else {
+    right();
+    delay(400);
   }
-  else
-  {
-    turnRightDistance(15);
+
+  forward();
+}
+
+void routeHome() {
+  moveForward(5000);
+  turnRight(700);
+  moveForward(3000);
+  stopCar();
+}
+
+void routeSchool() {
+  moveForward(3000);
+  turnLeft(600);
+  moveForward(2000);
+  stopCar();
+}
+
+void routeOffice() {
+  turnRight(800);
+  moveForward(2500);
+  turnLeft(500);
+  moveForward(2000);
+  stopCar();
+}
+
+void routeHospital() {
+  moveForward(2000);
+  turnLeft(700);
+  moveForward(4000);
+  turnRight(600);
+  moveForward(1000);
+  stopCar();
+}
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_CAR");
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  scanner.attach(SERVO_PIN);
+  scanner.write(90);
+
+  ledcSetup(CH_A, 5000, 8);
+  ledcAttachPin(ENA, CH_A);
+
+  ledcSetup(CH_B, 5000, 8);
+  ledcAttachPin(ENB, CH_B);
+
+  setSpeed(motorSpeed);
+}
+
+void loop() {
+  if (SerialBT.available()) {
+    char cmd = SerialBT.read();
+    lastCmdTime = millis();
+
+    if (cmd == 'M') {
+      autoMode = false;
+      routeMode = false;
+      routeRunning = false;
+      stopCar();
+    }
+
+    if (cmd == 'A') {
+      autoMode = true;
+      routeMode = false;
+      routeRunning = false;
+    }
+
+    if (cmd == '1') { routeMode = true; autoMode = false; routeID = 1; routeRunning = false; }
+    if (cmd == '2') { routeMode = true; autoMode = false; routeID = 2; routeRunning = false; }
+    if (cmd == '3') { routeMode = true; autoMode = false; routeID = 3; routeRunning = false; }
+    if (cmd == '4') { routeMode = true; autoMode = false; routeID = 4; routeRunning = false; }
+
+    if (!autoMode && !routeMode) {
+      if (cmd == 'F') forward();
+      if (cmd == 'B') backward();
+      if (cmd == 'L') left();
+      if (cmd == 'R') right();
+      if (cmd == 'S') stopCar();
+    }
   }
-}
 
-void runSchoolRoute()
-{
-  moveForwardDistance(100);
-  moveForwardDistance(100);
-  moveForwardDistance(100);
-}
+  if (millis() - lastCmdTime > 2000) {
+    stopCar();
+  }
 
-void runHomeRoute()
-{
-  moveForwardDistance(100);
-  turnLeftDistance(15);
-  moveForwardDistance(80);
-  turnRightDistance(15);
-  moveForwardDistance(100);
-}
+  if (autoMode) {
+    autoDrive();
+  }
 
-void runOfficeRoute()
-{
-  moveForwardDistance(120);
-  turnLeftDistance(15);
-  moveForwardDistance(80);
-  turnRightDistance(15);
-  moveForwardDistance(90);
-  turnLeftDistance(15);
-}
+  if (routeMode && !routeRunning) {
+    routeRunning = true;
 
-void runHospitalRoute()
-{
-  moveForwardDistance(100);
-  turnLeftDistance(15);
-  moveForwardDistance(100);
-  turnRightDistance(15);
-  moveForwardDistance(100);
-}
+    if (routeID == 1) routeHome();
+    if (routeID == 2) routeSchool();
+    if (routeID == 3) routeOffice();
+    if (routeID == 4) routeHospital();
 
-void stopRobot()
-{
-  digitalWrite(motorLeft1, LOW);
-  digitalWrite(motorLeft2, LOW);
-  digitalWrite(motorRight1, LOW);
-  digitalWrite(motorRight2, LOW);
-  analogWrite(speedA, 0);
-  analogWrite(speedB, 0);
+    routeRunning = false;
+    routeMode = false;
+  }
 }
